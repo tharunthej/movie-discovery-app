6d564475b280201977d7a8d7493368118e3d443e
tharunthej <tharunthej989@gmail.com>
2025-06-14 13:21:18 +0530

    clean up

--- movie-discovery-app/src/main.tsx
+++ movie-discovery-app/src/main.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import { store } from './redux/store'
 import App from './App'
 import './index.css'
+import './App.css'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
